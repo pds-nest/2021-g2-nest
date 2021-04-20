d9b9297c90314c4fc215625d059d5e2d1c60014d
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-21 00:23:35 +0200

    ✨ Setup basic theme structure

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,26 +1,14 @@
-import logo from "./logo.svg"
-import "./App.css"
+import {useState} from "react"
+import classNames from "classnames"
+import Style from "./App.module.css"
 
 
-function App() {
+export default function App() {
+    const [theme, setTheme] = useState("theme-dark");
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className={classNames(Style["app"], theme)}>
+            Ciao mondo!
         </div>
     )
 }
-
-export default App
