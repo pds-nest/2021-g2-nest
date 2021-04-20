7bbd0a84214659e2c1de004a5e8e67d60688b863
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-21 01:22:31 +0200

    🔧 Allow BoxWithHeader to accept nodes instead of objects as props

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -10,41 +10,29 @@ export default function App() {
     return (
         <div className={classNames(Style.App, theme)}>
             <BoxWithHeader
-                header={{
-                    children: "Ciao mondo!"
-                }}
-                body={{
-                    children: (
+                header={"Ciao mondo!"}
+                body={
+                    <div>
                         <div>
-                            <div>
-                                Il CSS è pura magia.
-                            </div>
-                            <div>
-                                Change my mind.
-                            </div>
+                            Il CSS è pura magia.
                         </div>
-                    ),
-                }}
+                        <div>
+                            Change my mind.
+                        </div>
+                    </div>
+                }
             />
             <BoxWithHeader
-                header={{
-                    children: "Questa è un'altra Box."
-                }}
-                body={{
-                    children: (
-                        <div>
-                            E altro testo.
-                        </div>
-                    )
-                }}
+                header={"Questa è un'altra Box."}
+                body={
+                    <div>
+                        E altro testo.
+                    </div>
+                }
             />
             <BoxWithHeader
-                header={{
-                    children: "Ecco, così va meglio.",
-                }}
-                body={{
-                    children: "No."
-                }}
+                header={"Ecco, così va meglio."}
+                body={"No."}
             />
         </div>
     )
--- code/frontend/src/components/BoxWithHeader.js
+++ code/frontend/src/components/BoxWithHeader.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React, {isValidElement} from "react"
 import Style from "./BoxWithHeader.module.css"
 import classNames from "classnames"
+import isString from "is-string"
 
 
-export default function BoxWithHeader({ header, body, className, ...props }) {
+export default function BoxWithHeader({ header, body, children, className, ...props }) {
+
+    if(isValidElement(header) || isString(header)) {
+        header = {
+            children: header
+        }
+    }
+
+    if(isValidElement(body) || isString(body)) {
+        body = {
+            children: body
+        }
+    }
+
+    if(children) {
+        if(body.children) {
+            throw new Error("If directly passing `children` to BoxWithHeader, body.children should not be set.")
+        }
+
+        body["children"] = children
+    }
+
     return (
         <div className={classNames(Style.BoxWithHeader, className)} {...props}>
             <div className={classNames(Style.BoxHeader, header.className)}>
