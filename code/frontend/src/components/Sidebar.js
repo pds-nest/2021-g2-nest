6017285a1852914330ded19b2ee7791c46528a0e
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-21 15:16:08 +0200

    🔧 Add logo placeholder to the sidebar

--- code/frontend/src/components/Sidebar.js
+++ code/frontend/src/components/Sidebar.js
@@ -6,6 +6,8 @@ import classNames from "classnames"
 export default function Sidebar({ children, className, ...props }) {
     return (
         <div className={classNames(Style.Sidebar, className)} {...props}>
+            {/* TODO: Aggiungere il logo qui! */}
+            <img src={""} width={230} height={170}/>
             {children}
         </div>
     )
