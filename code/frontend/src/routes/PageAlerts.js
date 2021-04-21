e1d3d5c20d1b46156b657633288828c1f3609336
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-21 18:32:36 +0200

    🔧 Add layout to PageAlerts

--- code/frontend/src/routes/PageAlerts.js
+++ code/frontend/src/routes/PageAlerts.js
@@ -1,12 +1,18 @@
 import React from "react"
 import Style from "./PageAlerts.module.css"
 import classNames from "classnames"
+import BoxWithHeader from "../components/BoxWithHeader"
 
 
 export default function PageAlerts({ children, className, ...props }) {
     return (
         <div className={classNames(Style.PageAlerts, className)} {...props}>
-            {children}
+            <BoxWithHeader header={"Your alerts"} className={Style.YourAlerts}>
+                a
+            </BoxWithHeader>
+            <BoxWithHeader header={"Create new alert"} className={Style.CreateAlert}>
+                b
+            </BoxWithHeader>
         </div>
     )
 }
