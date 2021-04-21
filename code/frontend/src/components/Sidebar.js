2cc4ba56ca3d2f8e5dc281559fab7a058879c276
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-21 15:54:52 +0200

    ✨ Add multiple buttons to the Sidebar

--- code/frontend/src/components/Sidebar.js
+++ code/frontend/src/components/Sidebar.js
@@ -3,7 +3,7 @@ import Style from "./Sidebar.module.css"
 import classNames from "classnames"
 import Logo from "./Logo"
 import ButtonSidebar from "./ButtonSidebar"
-import { faHome } from "@fortawesome/free-solid-svg-icons"
+import { faCog, faExclamationTriangle, faFolder, faHome } from "@fortawesome/free-solid-svg-icons"
 
 
 export default function Sidebar({ children, className, ...props }) {
@@ -12,6 +12,9 @@ export default function Sidebar({ children, className, ...props }) {
             {/* TODO: Aggiungere il logo qui! */}
             <Logo/>
             <ButtonSidebar icon={faHome}>Dashboard</ButtonSidebar>
+            <ButtonSidebar icon={faFolder}>Repositories</ButtonSidebar>
+            <ButtonSidebar icon={faExclamationTriangle}>Alerts</ButtonSidebar>
+            <ButtonSidebar icon={faCog}>Settings</ButtonSidebar>
             {children}
         </div>
     )
