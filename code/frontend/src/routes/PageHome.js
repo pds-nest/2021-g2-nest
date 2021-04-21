24f70457b626b6d7f265f372664ab05d7cccfa0e
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-21 18:46:35 +0200

    🔧 Add layout to PageHome

--- code/frontend/src/routes/PageHome.js
+++ code/frontend/src/routes/PageHome.js
@@ -1,12 +1,28 @@
 import React from "react"
 import Style from "./PageHome.module.css"
 import classNames from "classnames"
+import BoxHeaderOnly from "../components/BoxHeaderOnly"
+import BoxWithHeader from "../components/BoxWithHeader"
 
 
 export default function PageHome({ children, className, ...props }) {
     return (
         <div className={classNames(Style.PageHome, className)} {...props}>
-            {children}
+            <BoxHeaderOnly className={Style.Header}>
+                Create a new repository
+            </BoxHeaderOnly>
+            <BoxWithHeader className={Style.SearchByZone} header={"Search by zone"}>
+
+            </BoxWithHeader>
+            <BoxWithHeader className={Style.SearchByHashtags} header={"Search by hashtags"}>
+
+            </BoxWithHeader>
+            <BoxWithHeader className={Style.SearchByTimePeriod} header={"Search by time period"}>
+
+            </BoxWithHeader>
+            <BoxWithHeader className={Style.CreateDialog} header={"Create repository"}>
+
+            </BoxWithHeader>
         </div>
     )
 }
