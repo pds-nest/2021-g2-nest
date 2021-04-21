bc2e487e31292d22ac8444d42c19e81d8d7d3a04
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-21 18:32:43 +0200

    🔧 Add layout to PageRepositories

--- code/frontend/src/routes/PageRepositories.js
+++ code/frontend/src/routes/PageRepositories.js
@@ -1,12 +1,18 @@
 import React from "react"
 import Style from "./PageRepositories.module.css"
 import classNames from "classnames"
+import BoxWithHeader from "../components/BoxWithHeader"
 
 
 export default function PageRepositories({ children, className, ...props }) {
     return (
         <div className={classNames(Style.PageRepositories, className)} {...props}>
-            {children}
+            <BoxWithHeader header={"Your active repositories"} className={Style.ActiveRepositories}>
+                a
+            </BoxWithHeader>
+            <BoxWithHeader header={"Your archived repositories"} className={Style.ArchivedRepositories}>
+                b
+            </BoxWithHeader>
         </div>
     )
 }
