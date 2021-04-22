f825e00c57c65da7bf8eb0cc638d533a6d822e10
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-22 19:10:36 +0200

    ✨ Add Create Repository box

--- code/frontend/src/routes/PageHome.js
+++ code/frontend/src/routes/PageHome.js
@@ -3,6 +3,13 @@ import Style from "./PageHome.module.css"
 import classNames from "classnames"
 import BoxHeaderOnly from "../components/BoxHeaderOnly"
 import BoxWithHeader from "../components/BoxWithHeader"
+import Checkbox from "../components/Checkbox"
+import InputWithIcon from "../components/InputWithIcon"
+import { faFolder, faPlus } from "@fortawesome/free-solid-svg-icons"
+import Radio from "../components/Radio"
+import Button from "../components/Button"
+import LabelledForm from "../components/LabelledForm"
+import Label from "../components/Label"
 
 
 export default function PageHome({ children, className, ...props }) {
@@ -11,17 +18,39 @@ export default function PageHome({ children, className, ...props }) {
             <BoxHeaderOnly className={Style.Header}>
                 Create a new repository
             </BoxHeaderOnly>
-            <BoxWithHeader className={Style.SearchByZone} header={"Search by zone"}>
+            <BoxWithHeader className={Style.SearchByZone} header={
+                <span><Checkbox/> Search by zone</span>
+            }>
 
             </BoxWithHeader>
-            <BoxWithHeader className={Style.SearchByHashtags} header={"Search by hashtags"}>
+            <BoxWithHeader className={Style.SearchByHashtags} header={
+                <span><Checkbox/> Search by hashtag</span>
+            }>
 
             </BoxWithHeader>
-            <BoxWithHeader className={Style.SearchByTimePeriod} header={"Search by time period"}>
+            <BoxWithHeader className={Style.SearchByTimePeriod} header={
+                <span><Checkbox/> Search by time period</span>
+            }>
 
             </BoxWithHeader>
             <BoxWithHeader className={Style.CreateDialog} header={"Create repository"}>
-
+                <LabelledForm action={""}>
+                    <Label for_={"repo-name"} text={"Repository name"}>
+                        <InputWithIcon id={"repo-name"} icon={faFolder}/>
+                    </Label>
+                    <Label for={"filter-mode"} text={"Add tweets if they satisfy"}>
+                        <label>
+                            <Radio name={"filter-mode"} value={"or"}/> At least one filter
+                        </label>
+                        &nbsp;
+                        <label>
+                            <Radio name={"filter-mode"} value={"and"}/> Every filter
+                        </label>
+                    </Label>
+                    <Button style={{"grid-column": "1 / 3"}} icon={faPlus} color={"Green"}>
+                        Create repository
+                    </Button>
+                </LabelledForm>
             </BoxWithHeader>
         </div>
     )
