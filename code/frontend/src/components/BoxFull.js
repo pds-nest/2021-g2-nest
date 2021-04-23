6d6825874fcb2e305f0888b99938c9f9d3ef0db1
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-23 19:07:50 +0200

    ✨ Add more props to BoxFull

--- code/frontend/src/components/BoxFull.js
+++ code/frontend/src/components/BoxFull.js
@@ -7,19 +7,34 @@ import classNames from "classnames"
  * A box with both a header and a body.
  *
  * @param header - The contents of the box header.
+ * @param headerClassName - Additional class(es) added to the inner `<div>` acting as the header.
+ * @param headerProps - Additional props passed to the inner `<div>` acting as the header.
  * @param children - The contents of the box body.
+ * @param childrenClassName - Additional class(es) added to the inner `<div>` acting as the body.
+ * @param childrenProps - Additional props passed to the inner `<div>` acting as the body.
  * @param className - Additional class(es) that should be added to the outer `<div>` of the box.
  * @param props - Additional props to pass to the box.
  * @returns {JSX.Element}
  * @constructor
  */
-export default function BoxFull({ header, children, className, ...props }) {
+export default function BoxFull(
+    {
+        header,
+        headerClassName,
+        headerProps,
+        children,
+        childrenClassName,
+        childrenProps,
+        className,
+        ...props
+    }) {
+
     return (
         <div className={classNames(Style.BoxWithHeader, className)} {...props}>
-            <div className={Style.BoxHeader}>
+            <div className={classNames(Style.BoxHeader, headerClassName)} {...headerProps}>
                 {header}
             </div>
-            <div className={Style.BoxBody}>
+            <div className={classNames(Style.BoxBody, childrenClassName)} {...childrenProps}>
                 {children}
             </div>
         </div>
