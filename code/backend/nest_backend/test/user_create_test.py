eae6e3eb17a683d4366108cde58dcf8776bd7017
stefanogoldoni <stefanogoldoni@gmail.com>
2021-04-26 23:19:17 +0200

    aggiunto verifica creazione utente

---
+++ code/backend/nest_backend/test/user_create_test.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+auth_code = ""
+
+
+def test_user_create():
+    global auth_code
+
+    r = requests.post('http://localhost:5000/api/login', json={'email': 'admin@admin.com', 'password': 'password'})
+    j = json.loads(r.text)
+    assert j['result'] == "success"
+    auth_code = j['data']['access_token']
+
+    r = requests.post(f'http://localhost:5000/api/user/create', headers={'authorization': "Bearer " + auth_code},
+                      json={'email': 'utente5@nest.com', 'password': '1234', 'username': 'utente5'})
+    j = json.loads(r.text)
+    assert j['result'] == "success"
+
+    auth_code = j['data']['access_token']
+    print("User_create eseguito correttamente!")
+
+
+print("Testing del metodo user_create")
+test_user_create()
