6d62532a517e98a9a03fe1b6114797b8bf50abc2
Annielated <giovannianniballi@hotmail.com>
2021-04-27 23:17:32 +0200

    user_create testata e funzionante

--- code/backend/nest_backend/test/user_create_test.py
+++ code/backend/nest_backend/test/user_create_test.py
@@ -13,11 +13,10 @@ def test_user_create():
     auth_code = j['data']['access_token']
 
     r = requests.post(f'http://localhost:5000/api/user/create', headers={'authorization': "Bearer " + auth_code},
-                      json={'email': 'utente5@nest.com', 'password': '1234', 'username': 'utente5'})
+                      json={'email': 'utente11@nest.com', 'password': '1234', 'username': 'utente11'})
     j = json.loads(r.text)
     assert j['result'] == "success"
 
-    auth_code = j['data']['access_token']
     print("User_create eseguito correttamente!")
 
 
