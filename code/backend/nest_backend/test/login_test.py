8128b4622e1572de878021d32ba7ca47f8096113
Giovanni Anniballi <253150@studenti.unimore.it>
2021-04-28 21:17:05 +0000

    importato unittest

--- code/backend/nest_backend/test/login_test.py
+++ code/backend/nest_backend/test/login_test.py
@@ -1,23 +1,25 @@
+import unittest
 import requests
 import json
 
 auth_code = ""
 
 
-def test_login():
-    global auth_code
-    r = requests.post('http://localhost:5000/api/login', json={'email': 'admin@admin.com', 'password': 'amogus'})
-    j = json.loads(r.text)
-    assert j['result'] == "failure"
-    r = requests.post('http://localhost:5000/api/login', json={'email': '', 'password': ''})
-    j = json.loads(r.text)
-    assert j['result'] == "failure"
-    r = requests.post('http://localhost:5000/api/login', json={'email': 'admin@admin.com', 'password': 'password'})
-    j = json.loads(r.text)
-    assert j['result'] == "success"
-    auth_code = j['data']['access_token']
-    print("Login eseguito correttamente!")
+class MyTestCase(unittest.TestCase):
+    def test_login(self):
+        global auth_code
+        r = requests.post('http://localhost:5000/api/login', json={'email': 'admin@admin.com', 'password': 'amogus'})
+        j = json.loads(r.text)
+        assert j['result'] == "failure"
+        r = requests.post('http://localhost:5000/api/login', json={'email': '', 'password': ''})
+        j = json.loads(r.text)
+        assert j['result'] == "failure"
+        r = requests.post('http://localhost:5000/api/login', json={'email': 'admin@admin.com', 'password': 'password'})
+        j = json.loads(r.text)
+        assert j['result'] == "success"
+        auth_code = j['data']['access_token']
+        print("Login eseguito correttamente!")
 
 
 print("Testing del login")
-test_login()
+
