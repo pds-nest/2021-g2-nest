0b8f9a6818e0b048f2d33a3cbbb3c09d220283e9
Stefano Pigozzi <256895@studenti.unimore.it>
2021-04-29 04:02:08 +0200

    🧹 Reformat code

--- code/backend/nest_backend/database/tables/Repository.py
+++ code/backend/nest_backend/database/tables/Repository.py
@@ -1,5 +1,5 @@
 """
-This module defines the Repository database class.
+This module defines the :class:`.Repository` database class.
 """
 
 from ..base import Base
@@ -7,13 +7,17 @@ from ..base import Base
 
 class Repository(Base.Model):
     __tablename__ = "repository"
+
+    # Columns
     id = Base.Column(Base.Integer, primary_key=True)
     name = Base.Column(Base.String, nullable=False)
     start = Base.Column(Base.DateTime, nullable=True)
     end = Base.Column(Base.DateTime, nullable=True)
     isActive = Base.Column(Base.Boolean, nullable=False, default=False)
+
     # Foreign Keys
     owner_id = Base.Column(Base.String, Base.ForeignKey("user.email"), nullable=False)
+
     # Relationships
     owner = Base.relationship("User", back_populates="owner_of")
     authorizations = Base.relationship("Authorization", back_populates="repository")
@@ -22,6 +26,11 @@ class Repository(Base.Model):
     uses = Base.relationship("Uses", back_populates="repository")
 
     def to_json(self):
-        return {"id": self.id, "name": self.name, "start": (self.start.isoformat() if self.start else None),
-                "isActive": self.isActive, "end": (self.end.isoformat() if self.end else None),
-                "owner": self.owner.to_json()}
+        return {
+            "id": self.id,
+            "name": self.name,
+            "start": self.start.isoformat() if self.start else None,
+            "isActive": self.isActive,
+            "end": self.end.isoformat() if self.end else None,
+            "owner": self.owner.to_json()
+        }
