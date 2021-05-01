a14a44f54653f85714837c0046f44f0b83c575dd
stefanogoldoni <stefanogoldoni@gmail.com>
2021-05-01 18:18:13 +0200

    test dei metodi get, delete e patch

--- code/backend/nest_backend/test/user_test.py
+++ code/backend/nest_backend/test/user_test.py
@@ -10,7 +10,8 @@ class MyTestCase(unittest.TestCase):
         global auth_code
 
         # sono un utente normale
-        r = requests.post('http://localhost:5000/api/v1/login', json={'email': 'utente_test@nest.com', 'password': 'password'})
+        r = requests.post('http://localhost:5000/api/v1/login',
+                          json={'email': 'utente_test@nest.com', 'password': 'password'})
         j = json.loads(r.text)
         assert j['result'] == "success"
         auth_code = j['data']['access_token']
@@ -24,7 +25,8 @@ class MyTestCase(unittest.TestCase):
         print("delete User NON eseguito correttamente!")
 
         # adesso divento admin
-        r = requests.post('http://localhost:5000/api/v1/login', json={'email': 'admin@admin.com', 'password': 'password'})
+        r = requests.post('http://localhost:5000/api/v1/login',
+                          json={'email': 'admin@admin.com', 'password': 'password'})
         j = json.loads(r.text)
         assert j['result'] == "success"
         auth_code = j['data']['access_token']
@@ -44,21 +46,40 @@ class MyTestCase(unittest.TestCase):
 
         print("delete User NON eseguito correttamente!")
 
-        r = requests.delete(f'http://localhost:5000/api/v1/users/utente3@nest.com',
+        r = requests.delete(f'http://localhost:5000/api/v1/users/utente13@nest.com',
                             headers={'authorization': "Bearer " + auth_code})
         j = json.loads(r.text)
         assert j['result'] == "success"
 
-        # TODO AGGIUNGERE TESTING DI GET E PATCH
+        print("delete User eseguito correttamente!")
+
+        # TODO: AGGIUNGERE TESTING DI GET E PATCH
 
         # chiedo le info di un utente che esiste
         r = requests.get(f'http://localhost:5000/api/v1/users/utente_test@nest.com',
-                            headers={'authorization': "Bearer " + auth_code})
+                         headers={'authorization': "Bearer " + auth_code})
         j = json.loads(r.text)
         assert j['result'] == "success"
 
-        print("User_delete eseguito correttamente!")
+        print("get User info eseguito correttamente!")
 
-    print("Testing del metodo user delete")
+        # chiedo le info di un utente che NON esiste
+        r = requests.get(f'http://localhost:5000/api/v1/users/utente_chenonesiste@nest.com',
+                         headers={'authorization': "Bearer " + auth_code})
+        j = json.loads(r.text)
+        assert j['result'] == "failure"
 
+        print("get User info NON eseguito correttamente!")
 
+        # modifico le info di un utente
+        r = requests.patch(f'http://localhost:5000/api/v1/users/utente1@nest.com',
+                           headers={'authorization': "Bearer " + auth_code},
+                           json={'username': 'utente1_modificato', 'password': 'password'})
+        j = json.loads(r.text)
+        assert j['result'] == "success"
+
+        print("patch User info eseguito correttamente!")
+
+        print("Tutti i test eseguiti correttamente!")
+
+    print("Testing dei metodo user delete, get e patch")
