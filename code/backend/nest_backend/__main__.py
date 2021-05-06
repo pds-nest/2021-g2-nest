7f2b36f1b5dc60ef53f30156365cf8082eb367e1
Lorenzo Balugani <lorenzo.balugani@gmail.com>
2021-05-06 17:31:49 +0200

    Relative imports fun

--- code/backend/nest_backend/__main__.py
+++ code/backend/nest_backend/__main__.py
@@ -10,7 +10,7 @@ from .gestione import *
 from flask_cors import CORS
 from flask_jwt_extended import *
 from .app import app
-from api_spec import spec
+from .api_spec import spec
 from swagger import swagger_ui_blueprint, SWAGGER_URL
 
 Base.init_app(app=app)
