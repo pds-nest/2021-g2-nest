2593da860266c31e95e46e931c1c171151252ba1
Stefano Pigozzi <256895@studenti.unimore.it>
2021-05-11 00:16:02 +0200

    🐛 Fix bug test

--- code/backend/nest_backend/test/conftest.py
+++ code/backend/nest_backend/test/conftest.py
@@ -4,6 +4,4 @@ Pytest configuration file.
 Import global fixtures here.
 """
 
-from fixtures.flask_client import flask_client, admin_access_token, admin_headers, user_access_token, user_headers, user_create
-
-
+from fixtures.flask_client import flask_client, admin_access_token, admin_headers, user_access_token, user_headers, user_exists
