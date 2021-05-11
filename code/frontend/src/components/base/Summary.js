adba8507cf1d472cb4793636030c8fced00382c9
Stefano Pigozzi <256895@studenti.unimore.it>
2021-05-11 18:27:11 +0200

    📔 Decorate the Summary component

--- code/frontend/src/components/base/Summary.js
+++ code/frontend/src/components/base/Summary.js
@@ -4,6 +4,21 @@ import classNames from "classnames"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 
+/**
+ * A long line displaying the summary of a certain entity, such as a repository or an user.
+ *
+ * @param title - The title of the summary.
+ * @param subtitle - The subtitle of the summary.
+ * @param upperLabel - The label for the upper value.
+ * @param upperValue - The upper value.
+ * @param lowerLabel - The label for the lower value.
+ * @param lowerValue - The lower value.
+ * @param buttons - Buttons to add to the right of the summary.
+ * @param className - Additional class(es) to add to the summary.
+ * @param props - Additional props to pass to the summary.
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function Summary(
     { title, subtitle, upperLabel, upperValue, lowerLabel, lowerValue, buttons, className, ...props },
 ) {
