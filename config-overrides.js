4a7fadbeb5615a2017b943c0ef9fa528970be300
Stefano Pigozzi <me@steffo.eu>
2021-05-14 00:02:24 +0200

    🔨 Fix frontend test framework

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,17 @@
 const path = require('path');
 
 module.exports = {
-    paths: function (paths, env) {
+    paths: (paths, env) => {
         paths.appIndexJs = path.resolve(__dirname, 'nest_frontend/index.js');
         paths.appSrc = path.resolve(__dirname, 'nest_frontend');
         paths.appPublic = path.resolve(__dirname, 'nest_frontend/public');
         return paths;
     },
+
+    jest: (config) => {
+        config.roots = config.roots.map(root => root.replace("src", "nest_frontend"))
+        config.collectCoverageFrom = config.collectCoverageFrom.map(root => root.replace("src", "nest_frontend"))
+        config.testMatch = config.testMatch.map(root => root.replace("src", "nest_frontend"))
+        return config;
+    }
 }
