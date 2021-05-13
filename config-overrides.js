e9922fb483d5b16396fc1e9367fa645628e594be
Stefano Pigozzi <me@steffo.eu>
2021-05-13 23:50:41 +0200

    🔨 Move around some more things

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,7 @@ module.exports = {
     paths: function (paths, env) {
         paths.appIndexJs = path.resolve(__dirname, 'nest_frontend/index.js');
         paths.appSrc = path.resolve(__dirname, 'nest_frontend');
+        paths.appPublic = path.resolve(__dirname, 'nest_frontend/public');
         return paths;
     },
 }
