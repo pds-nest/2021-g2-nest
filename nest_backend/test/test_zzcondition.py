29ec779203cd5ebd7c45731c25eec587cf29c84c
stefanogoldoni <stefanogoldoni@gmail.com>
2021-05-15 12:00:49 +0200

    completati test PATCH e DELETE conditions

--- nest_backend/test/test_zzcondition.py
+++ nest_backend/test/test_zzcondition.py
@@ -33,32 +33,54 @@ class TestConditionPost:
     def test_wrong_type(self, flask_client: Client, user_headers):
         r = flask_client.post(f'/api/v1/repositories/1/conditions', headers=user_headers,
                               json={"content": "string", "type": 99})
-        assert r.status_code == 400 # ottengo 500
+        assert r.status_code == 400
         assert r.json["result"] == "failure"
 
     def test_missing_content(self, flask_client: Client, user_headers):
         r = flask_client.post(f'/api/v1/repositories/1/conditions', headers=user_headers,
                               json={"type": 0})
-        assert r.status_code == 400 # ottengo 500
+        assert r.status_code == 400
         assert r.json["result"] == "failure"
 
     def test_for_success(self, flask_client: Client, user_headers):
         r = flask_client.post(f'/api/v1/repositories/1/conditions', headers=user_headers,
-                              json={"content": "string", "type": 0})
+                              json={"content": "onlyForTest", "type": 0})
         assert r.status_code == 201
         assert r.json["result"] == "success"
 
     # test del file condition
 
 
-class TestConditionGetOneOfARepository:
-    def test_condition_not_found(selfself, flask_client: Client, user_headers):
-        r = flask_client.get(f'/api/v1/conditions/999', headers=user_headers)
+class TestOneConditionOfARepository:
+    def test_condition_not_found(self, flask_client: Client, user_headers):
+        r = flask_client.get(f'/api/v1/conditions/99', headers=user_headers)
         assert r.status_code == 404
         assert r.json["result"] == "failure"
 
-    # TODO: testare  GET
+    # test GET
+    def test_get_condition(self, flask_client: Client, user_headers):
+        r = flask_client.get(f'/api/v1/conditions/3', headers=user_headers)
+        assert r.status_code == 200
+        assert r.json["result"] == "success"
 
-    # TODO: testare PATCH
+    # test PATCH
+    def test_patch_condition_no_json(self, flask_client: Client, user_headers):
+        r = flask_client.patch(f'/api/v1/conditions/3', headers=user_headers)
+        assert r.status_code == 400
+        assert r.json["result"] == "failure"
 
-    # TODO: testare DELETE
+    def test_patch_condition_wrong_type(self, flask_client: Client, user_headers):
+        r = flask_client.patch(f'/api/v1/conditions/3', headers=user_headers,
+                               json={"content": "string", "type": 99})
+        assert r.status_code == 400
+        assert r.json["result"] == "failure"
+
+    def test_patch_condition_for_success(self, flask_client: Client, user_headers):
+        r = flask_client.patch(f'/api/v1/conditions/3', headers=user_headers,
+                               json={"content": "patchedForTest", "type": 0})
+        assert r.status_code == 204
+
+    # test DELETE
+    def test_delete_condition_for_success(self, flask_client: Client, user_headers):
+        r = flask_client.delete(f'/api/v1/conditions/3', headers=user_headers)
+        assert r.status_code == 204
