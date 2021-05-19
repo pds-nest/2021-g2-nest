5e24c8f2e65db88870fc875650c54640b922f8f3
Stefano Pigozzi <me@steffo.eu>
2021-05-19 20:16:23 +0200

    ✨ Add stats to the visualization selector

--- nest_frontend/components/interactive/PickerVisualization.js
+++ nest_frontend/components/interactive/PickerVisualization.js
@@ -1,6 +1,15 @@
 import React from "react"
 import ButtonIconOnly from "../base/ButtonIconOnly"
-import { faAt, faChartBar, faClock, faCloud, faHashtag, faMap, faMapPin } from "@fortawesome/free-solid-svg-icons"
+import {
+    faAt,
+    faChartBar,
+    faClock,
+    faCloud,
+    faHashtag,
+    faMap,
+    faMapPin,
+    faStar,
+} from "@fortawesome/free-solid-svg-icons"
 
 
 export default function PickerVisualization({ currentTab, setTab, ...props }) {
@@ -9,6 +18,7 @@ export default function PickerVisualization({ currentTab, setTab, ...props }) {
             <ButtonIconOnly onClick={() => setTab("wordcloud")} disabled={currentTab === "wordcloud"} color={"Grey"} icon={faCloud}/>
             <ButtonIconOnly onClick={() => setTab("histogram")} disabled={currentTab === "histogram"} color={"Grey"} icon={faChartBar}/>
             <ButtonIconOnly onClick={() => setTab("map")} disabled={currentTab === "map"} color={"Grey"} icon={faMap}/>
+            <ButtonIconOnly onClick={() => setTab("stats")} disabled={currentTab === "stats"} color={"Grey"} icon={faStar}/>
         </div>
     )
 }
